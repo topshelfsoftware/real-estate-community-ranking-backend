db0766975f3b3018156bc41928a3c7404eb05b5b
Tanner Pubins <t.pubins@icloud.com>
2023-06-27 23:19:13 -0700

    Simplify init code.

--- service/lambda/rank_communities/__init__.py
+++ service/lambda/rank_communities/__init__.py
@@ -1,14 +1,7 @@
 import os
-
-from topshelfsoftware_util import get_package_loggers
 # ----------------------------------------------------------------------------#
 #                               --- Globals ---                               #
 # ----------------------------------------------------------------------------#
-MODULE_NAME = "rank-communities"
+MODULE_NAME = "rank_communities"
 COMMUNITY_DATA_BUCKET_NAME = os.environ["COMMUNITY_DATA_BUCKET_NAME"]
 COMMUNITY_DATA_OBJECT_NAME = os.environ["COMMUNITY_DATA_OBJECT_NAME"]
-
-# ----------------------------------------------------------------------------#
-#                               --- Logging ---                               #
-# ----------------------------------------------------------------------------#
-loggers = get_package_loggers()
