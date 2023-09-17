62de7a3d0d8af04e845240c337f499c36fbad321
Tanner Pubins <t.pubins@icloud.com>
2023-09-17 09:50:37 -0700

    Initialize all Lambdas by adding log streams to util package loggers.

--- service/lambdas/validate_community_data/src/__init__.py
+++ service/lambdas/validate_community_data/src/__init__.py
@@ -1,4 +1,13 @@
+import topshelfsoftware_aws_util
+import topshelfsoftware_util
+from topshelfsoftware_util.log import add_log_stream
 # ----------------------------------------------------------------------------#
 #                               --- Globals ---                               #
 # ----------------------------------------------------------------------------#
 MODULE_NAME = "validate_community_data"
+
+# ----------------------------------------------------------------------------#
+#                               --- Logging ---                               #
+# ----------------------------------------------------------------------------#
+[add_log_stream(logger) for logger in topshelfsoftware_aws_util.get_package_loggers()]
+[add_log_stream(logger) for logger in topshelfsoftware_util.get_package_loggers()]
--- service/lambdas/validate_rank_inputs/src/__init__.py
+++ service/lambdas/validate_rank_inputs/src/__init__.py
@@ -1,6 +1,16 @@
 import os
+
+import topshelfsoftware_aws_util
+import topshelfsoftware_util
+from topshelfsoftware_util.log import add_log_stream
 # ----------------------------------------------------------------------------#
 #                               --- Globals ---                               #
 # ----------------------------------------------------------------------------#
 MODULE_NAME = "validate_rank_inputs"
 SRC_PATH = os.path.dirname(os.path.realpath(__file__))
+
+# ----------------------------------------------------------------------------#
+#                               --- Logging ---                               #
+# ----------------------------------------------------------------------------#
+[add_log_stream(logger) for logger in topshelfsoftware_aws_util.get_package_loggers()]
+[add_log_stream(logger) for logger in topshelfsoftware_util.get_package_loggers()]
